7fe0a9f5ac9535fe0bcc63dbdd17b16508b534da
Mirul Ansari <mirulkhan786@gmail.com>
2022-12-26 08:44:15 +0530

    modulo

---
+++ temp3.js
@@ -0,0 +1,12 @@
+let x = 10;
+let y = 8;
+
+if (x%2==0 && y%2==0){
+  console.log("Both are divisible by 2")
+}
+else if (x%5==0 && y%2==0){
+  console.log("I am getting remainder as 0")
+}
+else {
+  console.log("Nothing")
+}
